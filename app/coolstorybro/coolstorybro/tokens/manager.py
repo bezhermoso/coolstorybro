594c053d444f42096ae8c5c3a0286c406405cd3c
Bez Hermoso <bez@activelamp.com>
2015-01-05 14:12:25 -0800

    Added life-cycle webhooks to save/delete JWT tokens. Also added .gitignore.

---
+++ app/coolstorybro/coolstorybro/tokens/manager.py
@@ -0,0 +1,21 @@
+import os
+
+def set(client, jwt):
+    file = get_path(client)
+    f = open(file, 'w')
+    f.write(jwt)
+    f.close()
+    pass
+
+def get(client):
+    file = get_path(client)
+    f = open(file, 'r')
+    return f.readline()
+
+def delete(client):
+    file = get_path(client)
+    os.remove(file)
+    return True
+
+def get_path(client):
+    return os.path.join(os.path.dirname(__file__), '_tokens', client)
--- app/coolstorybro/coolstorybro/views.py
+++ app/coolstorybro/coolstorybro/views.py
@@ -2,7 +2,8 @@ from pyramid.view import view_config
 import os
 from pyramid.response import FileResponse
 from pyramid.response import Response
-
+from tokens import manager
+import json
 
 @view_config(route_name='home', renderer='templates/mytemplate.pt')
 def my_view(request):
@@ -10,16 +11,37 @@ def my_view(request):
 
 @view_config(name='atlassian-connect.json')
 def descriptor_view(request):
-    return FileResponse(os.path.join(os.path.dirname(__file__), 'descriptor.json'), request=request)
+    json_data = open(os.path.join(os.path.dirname(__file__), 'descriptor.json'))
+    data = json.load(json_data)
+    data['baseUrl'] = request.application_url
+    return Response(json.dumps(data), 200, content_type='application/json')
+
 
 # Add-on life-cycle callbacks
 
 @view_config(name="installed")
 def lifecycle_installed_view(request):
-    print request
-    return Response('Foo!')
+    data = request.json_body
+    if (data):
+        manager.set(data['clientKey'], data['sharedSecret'])
+        return Response('OK!')
+    else:
+        return Response('Invalid data.', 400)
 
 @view_config(name="uninstalled")
 def lifecycle_uninstalled_view(request):
+    print request
+    data = request.json_body
+    if (data):
+        manager.delete(data['clientKey'])
+        return Response('OK!')
+    else:
+        return Response('Invalid data.', 400)
+
+# Webhook callbacks
+@view_config(route_name="webhook")
+def webhook_view(request):
+    print request
+    return Response('OK!')
     pass
 
